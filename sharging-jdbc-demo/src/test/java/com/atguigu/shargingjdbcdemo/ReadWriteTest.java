bce5a7165940372a4bb5534efd3432eb30bc4bf8
zzz <751781196@qq.com>
2025-08-13 23:17:14 +0800

    feat(sharding-jdbc-demo): 添加分库分表配置和实体类
    - 新增多个分库分表配置文件，包括水平分片、垂直分片、读写分离等场景
    - 添加实体类 Order、OrderItem 和 Dict，用于演示分片功能
    - 新增对应的 Mapper 接口，用于操作数据库
    - 更新 .gitignore 文件，添加项目构建和 IDE 相关的忽略项
    - 添加 Maven Wrapper 配置文件和下载器，便于项目构建

---
+++ sharging-jdbc-demo/src/test/java/com/atguigu/shargingjdbcdemo/ReadWriteTest.java
@@ -0,0 +1,63 @@
+package com.atguigu.shargingjdbcdemo;
+
+import com.atguigu.shargingjdbcdemo.entity.Order;
+import com.atguigu.shargingjdbcdemo.entity.User;
+import com.atguigu.shargingjdbcdemo.mapper.OrderMapper;
+import com.atguigu.shargingjdbcdemo.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@SpringBootTest
+public class ReadWriteTest {
+
+    @Autowired
+    private UserMapper userMapper;
+    
+    @Autowired
+    private OrderMapper orderMapper;
+
+
+    /**
+     * 读写分离：写入数据的测试
+     */
+    @Test
+    public void testInsert(){
+
+        User user = new User();
+        user.setUname("张三丰");
+        userMapper.insert(user);
+    }
+
+    /**
+     * 读写分离：事务测试
+     */
+    @Transactional//开启事务
+    @Test
+    public void testTrans(){
+
+        User user = new User();
+        user.setUname("铁锤");
+        userMapper.insert(user);
+
+        List<User> users = userMapper.selectList(null);
+    }
+
+
+    /**
+     * 读写分离：负载均衡测试
+     */
+    @Test
+    public void testSelectAll(){
+
+        List<User> users1 = userMapper.selectList(null);
+        List<User> users2 = userMapper.selectList(null);
+        List<User> users3 = userMapper.selectList(null);
+        List<User> users4 = userMapper.selectList(null);
+//        users.forEach(System.out::println);
+    }
+}
