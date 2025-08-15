92315db83755fc127025c9f825f29fc566472d14
zhangzongzheng <751781196@qq.com>
2025-08-15 11:13:00 +0800

    1

--- my-demo/src/main/java/cn/zzz/demo/config/ShardingAlgorithm.java
+++ my-demo/src/main/java/cn/zzz/demo/config/ShardingAlgorithm.java
@@ -6,7 +6,9 @@ import com.google.common.collect.Range;
 import org.apache.shardingsphere.sharding.api.sharding.complex.ComplexKeysShardingAlgorithm;
 import org.apache.shardingsphere.sharding.api.sharding.complex.ComplexKeysShardingValue;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -21,7 +23,8 @@ public class ShardingAlgorithm implements ComplexKeysShardingAlgorithm<Comparabl
 
 
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
-    
+    private final String logicTableName = "fee";
+
     /**
      * 复杂分片
      *
@@ -32,45 +35,70 @@ public class ShardingAlgorithm implements ComplexKeysShardingAlgorithm<Comparabl
     public Collection<String> doSharding(Collection<String> availableTargetNames, ComplexKeysShardingValue<Comparable<?>> shardingValue) {
 
         Map<String, Collection<Comparable<?>>> columnNameAndShardingValuesMap = shardingValue.getColumnNameAndShardingValuesMap();
+        // 根据orderTime
         Collection<Comparable<?>> orderTimes = columnNameAndShardingValuesMap.get("order_time");
         if (orderTimes != null) {
             Set<String> expectTableNames = orderTimes.stream()
                     .filter(Objects::nonNull)
                     .map(ot -> ((LocalDateTime) ot).format(formatter))
-                    .map(s -> "fee_" + s)
+                    .map(this::getTableName)
                     .collect(Collectors.toSet());
             
             return availableTargetNames.stream().filter(expectTableNames::contains).toList();
         }
 
+        // 根据orderTime范围
+        Map<String, Range<Comparable<?>>> columnNameAndRangeValuesMap = shardingValue.getColumnNameAndRangeValuesMap();
+        if (columnNameAndRangeValuesMap != null) {
+            Range<Comparable<?>> orderTimeRange = columnNameAndRangeValuesMap.get("order_time");
+            if (orderTimeRange != null) {
+                LocalDateTime startTime = null, endTime = null;
+                if (orderTimeRange.hasLowerBound()) {
+                    startTime = (LocalDateTime) orderTimeRange.lowerEndpoint();
+                    BoundType lowerBoundType = orderTimeRange.lowerBoundType();
+                }
+                if (orderTimeRange.hasUpperBound()) {
+                    endTime = (LocalDateTime) orderTimeRange.upperEndpoint();
+                    BoundType upperBoundType = orderTimeRange.upperBoundType();
+                }
+                // todo 这里先写死 跟配置文件里对应
+                if (startTime == null) {
+                    startTime = LocalDateTime.of(LocalDate.of(2025, 1, 1), LocalTime.MIN);
+                }
+                if (endTime == null) {
+                    endTime = LocalDateTime.of(LocalDate.of(2025, 12, 31), LocalTime.MAX);
+                }
+
+                Set<String> expectTableNames = new HashSet<>();
+                LocalDateTime curr = startTime;
+                while (!curr.isAfter(endTime)) {
+                    String yearMonth = curr.format(formatter);
+                    expectTableNames.add(getTableName(yearMonth));
+                    curr = curr.plusMonths(1);
+                }
+
+                return availableTargetNames.stream().filter(expectTableNames::contains).toList();
+            }
+        }
+
+        // 根据orderNumber
         Collection<Comparable<?>> orderNumbers = columnNameAndShardingValuesMap.get("order_number");
         if (orderNumbers != null) {
             Set<String> expectTableNames = orderNumbers.stream()
                     .filter(Objects::nonNull)
                     .map(on -> OrderNumberGenerator.getYYYYMMFromOrderNumber((String) on))
-                    .map(s -> "fee_" + s)
+                    .map(this::getTableName)
                     .collect(Collectors.toSet());
 
             return availableTargetNames.stream().filter(expectTableNames::contains).toList();
         }
 
-        Map<String, Range<Comparable<?>>> columnNameAndRangeValuesMap = shardingValue.getColumnNameAndRangeValuesMap();
-        if (columnNameAndRangeValuesMap != null) {
-            Range<Comparable<?>> orderTimeRange = columnNameAndRangeValuesMap.get("order_time");
-            if (orderTimeRange != null) {
-                // TODO 2025/8/14
-                if (orderTimeRange.hasLowerBound()) {
-                    LocalDateTime lowerEndpoint = (LocalDateTime) orderTimeRange.lowerEndpoint();
-                    BoundType lowerBoundType = orderTimeRange.lowerBoundType();
-                }
-                if (orderTimeRange.hasUpperBound()) {
-                    LocalDateTime upperEndpoint = (LocalDateTime) orderTimeRange.upperEndpoint();
-                    BoundType upperBoundType = orderTimeRange.upperBoundType();
-                }
-            }
-        }
 
         return null;
+    }
+
+    private String getTableName(String yearMonth) {
+        return logicTableName + "_" + yearMonth;
     }
 
     @Override
